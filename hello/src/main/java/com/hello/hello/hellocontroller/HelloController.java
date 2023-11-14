dd69a64db16e7926129ab592b4a4fd2763922599
Kim Jae Hyuk <rlawogur816@gmail.com>
2023-11-14 15:57:47 +0900

    test

---
+++ hello/src/main/java/com/hello/hello/hellocontroller/HelloController.java
@@ -0,0 +1,14 @@
+package com.hello.hello.hellocontroller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/v1/hello")
+    public String hello(String hello){
+        hello = "hello";
+        return hello;
+    }
+}
